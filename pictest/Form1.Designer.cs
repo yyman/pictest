e64f600b846420a4698a464eb2ee54ac20af54c9
yyman <13fmi39@ms.dendai.ac.jp>
2014-01-30 02:49:51 +0900

    githubにコミット

---
+++ pictest/Form1.Designer.cs
@@ -0,0 +1,398 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 15);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(274, 367);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox2.Location = new System.Drawing.Point(3, 15);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(265, 366);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(20, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(77, 42);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Open";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Form1_Load);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 167);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Cut";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Cut_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(22, 374);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Save";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Location = new System.Drawing.Point(16, 60);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(92, 198);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Cut";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(4, 142);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(82, 19);
+            this.textBox4.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 127);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 12);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Height";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(5, 105);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(81, 19);
+            this.textBox3.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 12);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Width";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(4, 67);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(82, 19);
+            this.textBox2.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(12, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Y";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(4, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(82, 19);
+            this.textBox1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(12, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "X";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.groupBox2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.pictureBox1);
+            this.groupBox2.Location = new System.Drawing.Point(0, 32);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(280, 385);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Original Image";
+            this.groupBox2.SizeChanged += new System.EventHandler(this.Form1_gb2Sizechange);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label5.Location = new System.Drawing.Point(3, 370);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(223, 12);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "画像内をドラッグでも切り取り範囲を指定可能";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox3.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.groupBox3.Controls.Add(this.pictureBox2);
+            this.groupBox3.Location = new System.Drawing.Point(280, 32);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(271, 384);
+            this.groupBox3.TabIndex = 7;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Cut Image";
+            this.groupBox3.SizeChanged += new System.EventHandler(this.Form1_gb3Sizechange);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.comboBox1);
+            this.groupBox4.Controls.Add(this.button4);
+            this.groupBox4.Location = new System.Drawing.Point(16, 294);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(92, 74);
+            this.groupBox4.TabIndex = 2;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Gaussian";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "3",
+            "5",
+            "7",
+            "9"});
+            this.comboBox1.Location = new System.Drawing.Point(4, 18);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(82, 20);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(6, 43);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Gaussian";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(72, 8);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(470, 19);
+            this.textBox5.TabIndex = 0;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(8, 8);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(62, 12);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Image Path";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(22, 265);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "GrayScale";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.groupBox3);
+            this.panel1.Controls.Add(this.groupBox2);
+            this.panel1.Location = new System.Drawing.Point(112, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(552, 420);
+            this.panel1.TabIndex = 11;
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.textBox5);
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(552, 32);
+            this.panel2.TabIndex = 9;
+            // 
+            // panel3
+            // 
+            this.panel3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.panel3.Controls.Add(this.button5);
+            this.panel3.Controls.Add(this.groupBox4);
+            this.panel3.Controls.Add(this.groupBox1);
+            this.panel3.Controls.Add(this.button3);
+            this.panel3.Controls.Add(this.button1);
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(112, 416);
+            this.panel3.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(666, 419);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.SizeChanged += new System.EventHandler(this.Form1_gb2Sizechange);
+            this.SizeChanged += new System.EventHandler(this.Form1_gb3Sizechange);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+    }
+}
+
---
+++ pictest/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        Bitmap img,imgTrim,imgTemp;
+        int x, ix, y, iy, w, h;
+        String fn;
+        double p;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_gb2Sizechange(object sender, EventArgs e)
+        {
+            groupBox2.Size = new System.Drawing.Size(panel1.Width / 2, groupBox2.Size.Height);
+            groupBox2.Refresh();
+        }
+
+        private void Form1_gb3Sizechange(object sender, EventArgs e)
+        {
+            groupBox3.Location = new System.Drawing.Point(panel1.Width / 2, 32);
+            groupBox3.Size = new System.Drawing.Size(panel1.Width / 2, groupBox3.Size.Height);
+            groupBox3.Refresh();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            OpenFileDialog openDialog = new OpenFileDialog();
+            DialogResult result = openDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                fn = openDialog.SafeFileName;
+                img = new Bitmap(openDialog.FileName);
+                pictureBox1.Image = img;
+                textBox5.Text = openDialog.FileName;
+            }
+        }
+
+        private void Cut_Click(object sender, EventArgs e)
+        {
+            if (img == null) return;
+            x = int.Parse(textBox1.Text);
+            y = int.Parse(textBox2.Text);
+            w = int.Parse(textBox3.Text);
+            h = int.Parse(textBox4.Text);
+            if (w <= 0 || h <= 0) return;
+            imgTrim = new Bitmap(w, h);
+            imgTrim.SetResolution(img.HorizontalResolution, img.VerticalResolution);
+            Graphics gd = Graphics.FromImage(imgTrim);
+            Rectangle rc = new Rectangle(x, y, w, h);
+            gd.DrawImage(img, 0, 0, rc, GraphicsUnit.Pixel);//画像を切り取り
+
+            imgTemp = (Bitmap)img.Clone();
+            Graphics g = Graphics.FromImage(imgTemp);
+            rc = new Rectangle(x, y, w, h);
+            g.DrawRectangle(Pens.Red, rc);
+
+            pictureBox1.Image = imgTemp;
+            pictureBox2.Image = imgTrim;
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+            if (img == null) return;
+            SaveFileDialog saveDialog = new SaveFileDialog();
+            saveDialog.FileName = fn;
+            DialogResult result = saveDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                imgTrim.Save(saveDialog.FileName);
+            }
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (img == null) return;
+
+            double pAs = (double)pictureBox1.Height / (double)pictureBox1.Width;
+            double iAs = (double)img.Height / (double)img.Width;
+            x = e.X;
+            y = e.Y;
+            if (pAs < iAs)
+            {
+                p = ((double)pictureBox1.Height / (double)img.Height);
+                ix = (int)((x - (int)(((double)pictureBox1.Width - (double)img.Width * p) / 2.0)) * (1.0 / p));
+                iy = (int)(y * (1 / p));
+            }
+            else
+            {
+                p = ((double)pictureBox1.Width / (double)img.Width);
+                ix = (int)(x * (1 / p));
+                iy = (int)((y - (int)(((double)pictureBox1.Height - (double)img.Height * p) / 2.0)) * (1.0 / p));
+                //ピクセルを元の画像にもどさないといけない
+            }
+            textBox1.Text = ix.ToString();
+            textBox2.Text = iy.ToString();
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (img == null) return;
+            w = (int)((e.X - x) * (1.0 / p));
+            h = (int)((e.Y - y) * (1.0 / p));
+            imgTemp = (Bitmap)img.Clone();
+            Graphics g = Graphics.FromImage(imgTemp);
+            Rectangle rc = new Rectangle(ix, iy, w, h);
+            g.DrawRectangle(Pens.Red, rc);
+            pictureBox1.Image = imgTemp;
+            textBox3.Text = w.ToString();
+            textBox4.Text = h.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (img == null) return;
+            button5_Click(sender, e);
+            int nr = int.Parse(comboBox1.Text);
+            for (int i = 0; i < imgTrim.Width; i++)
+            for (int j = 0; j < imgTrim.Height; j++)
+            {
+                int nn= 0;//近傍数
+                int sum = 0;//平均値を求める合計
+                for (int ii = i - nr; ii <= i + nr; ii++)
+                {
+                    if (ii < 0) continue;//はみ出し処理
+                    if (ii >= imgTrim.Width) continue;//
+                    for (int jj = j - nr; jj <= j + nr; jj++)
+                    {
+                        if (jj < 0) continue;//はみ出し処理
+                        if (jj >= imgTrim.Height) continue;//
+                        sum += (int)imgTrim.GetPixel(ii, jj).R;
+                        nn++;
+                    }
+                }
+                int ave= sum / nn;//平均値を求める
+                Color col= Color.FromArgb(ave,ave,ave);//Colorに変換
+                imgTrim.SetPixel(i, j, col);
+            }
+            this.pictureBox2.Image = imgTrim;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (img == null) return;
+            int r, g, b, luminance;
+
+            if (imgTrim == null)
+            {
+                MessageBox.Show("画像を切り取ってから行って下さい。", "使用上の注意", MessageBoxButtons.OK);
+                return;
+            }
+
+            for (int i = 0; i < imgTrim.Width; i++)
+                for (int j = 0; j < imgTrim.Height; j++)
+                {
+                    r = (int)imgTrim.GetPixel(i, j).R;
+                    g = (int)imgTrim.GetPixel(i, j).G;
+                    b = (int)imgTrim.GetPixel(i, j).B;
+                    luminance = (int)(0.298912 * (double)r + 0.586611 * (double)g + 0.114478 * (double)b);
+                    Color col = Color.FromArgb(luminance, luminance, luminance);
+                    imgTrim.SetPixel(i, j, col);
+                }
+            this.pictureBox2.Image = imgTrim;
+        }
+    }
+}
